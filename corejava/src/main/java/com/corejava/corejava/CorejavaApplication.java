bd7aa0db87a4e391314e47aa148e71cb77a94bc3
homedew <homedew.tc@gmail.com>
2024-02-17 16:49:24 +0700

    Create project with java spring

---
+++ corejava/src/main/java/com/corejava/corejava/CorejavaApplication.java
@@ -0,0 +1,14 @@
+package com.corejava.corejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorejavaApplication.class, args);
+		System.out.println("hello world2");
+	}
+
+}
